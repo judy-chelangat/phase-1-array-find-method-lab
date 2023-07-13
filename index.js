0a1fef517a04ef05422d18cd6a1588a9181fcb91
judy-chelangat <judysigilai75@gmail.com>
2023-07-13 22:58:11 +0300

    completed assignemnt

--- index.js
+++ index.js
@@ -11,24 +11,24 @@ const record = [
 // It should use find() to test each Object to see if the result is "W" — a win!
 // It should return the year when the win occurred (if it occurred at all!)
 // If no win is found, it should return, sadly, undefined
-function superbowlWin(records){
+// function superbowlWin(records){
 
-if(records.result === "W"){
-    return records.year
-}
-else{
-    return undefined
-}
-}
-record.find(superbowlWin)
+// if(records.result === "W"){
+//     return records.year
+// }
+// else{
+//     return undefined
+// }
+// }
+// record.find(superbowlWin)
 
-// function superbowlWin(records) {
-//     const broncosWin = records.find(record => record.result === "W");
+function superbowlWin(records) {
+    const broncosWin = records.find(record => record.result === "W");
   
-//     if (broncosWin) {
-//       return broncosWin.year;
-//     } else {
-//       return undefined;
-//     }
-//   }
+    if (broncosWin) {
+      return broncosWin.year;
+    } else {
+      return undefined;
+    }
+  }
   
